9bc9669f62c7dfae99bb2e3a571c74d62e4751df
Jose Riguera <jriguera@gmail.com>
2016-04-11 01:41:06 +0200

    repository structure

---
+++ src/bosh_ironic_cpi/ironic_cpi/actions/repositories/repository.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+BOSH OpenStack Ironic CPI
+"""
+# Python 2 and 3 compatibility
+from __future__ import unicode_literals
+
+import logging
+
+from ironic_cpi.action import CPIActionError
+
+
+
+def ExceptionCatch(method):
+    """
+    Wrapper for methods to catch all Exceptions
+    """
+
+    def wrapped_method(self, *args, **kwargs):
+        try:
+            method(self, *args, **kwargs)
+        except Exception as e:
+            msg = str(e)
+            raise RepositoryError(msg)
+    return wrapped_method
+
+
+
+class RepositoryCatcher(type):
+    """
+    Metaclass to wrap all methods
+    """
+
+    def __init__(cls, name, bases, dct):
+        if not hasattr(cls, 'registry'):
+            # this is the base class.  Create an empty registry
+            cls.registry = {}
+        else:
+            # this is a derived class.  Add cls to the registry
+            # if the class has the protocol attribute
+            try:
+                cls.registry[cls.protocol] = cls
+            except:
+                pass
+        # Define a wrapper for each method to catch the Exceptions
+        for m in dct:
+            if hasattr(dct[m], '__call__'):
+                dct[m] = ExceptionCatch(dct[m])
+        super(RepositoryCatcher, cls).__init__(name, bases, dct)
+
+    # Metamethods, called on class objects:
+    def __getitem__(cls, key):
+        return cls.registry[key]
+
+    def __iter__(cls):
+        return iter(cls.registry)
+
+
+
+class Repository(object):
+    __metaclass__ = RepositoryCatcher
+    #protocol = ''
+
+    def __init__(self, config, logger=None):
+        self.config = config
+        self.log(logger)
+        self.logger.debug("Initializing class %s" % self.__class__.__name__)
+
+    def log(self, logger=None):
+        self.logger = logger
+        if not logger:
+            self.logger = logging.getLogger(self.__class__.__name__)
+    
+    def mkdir(self, path):
+        pass
+
+    def rmdir(self, path):
+        pas
+
+    def delete(self, path):
+        pass
+
+    def put(self, local, remote):
+        pass
+
+    def get(self, remote, local):
+        pass
+
+    def exists(self, remote):
+        pass
+
+
+
+class RepositoryError(Exception):
+    """
+    Base class for all Repository Exceptions
+    """
+    
+    def __init__(self, message):
+        self.message = message
+
+    def __repr__(self):
+        name = self.__class__.__name__        
+        show = "<%s (%s)>" % (name, self.message)
+        return show
+
+    def __str__(self):
+        return self.message
+
+
+
+class RepositoryManager(object):
+    _metarepository = Repository
+    repository_config_key = 'repository_type'
+
+    def __init__(self, logger=None):
+        self.logger = logger
+        if not logger:
+            self.logger = logging.getLogger(self.__class__.__name__)
+        self.repository = None
+
+    def manage(self, config):
+        self.repository = None
+        repository_type = config[self.repository_config_key]
+        for protocol in self._metarepository:
+            repo_class = self._metarepository[protocol]
+            msg = "Repository class for protocol %s: %s"
+            msg = msg % (protocol, repo_class.__name__)
+            self.logger.debug(msg)
+            if protocol == repository_type:
+                self.repository = repo_class(config, self.logger)
+        if not self.repository:
+            msg = "Protocol '%s' not supported!" % repository_type
+            long_msg = "Not found class to manage repository of type '%s'"
+            long_msg = long_msg % repository_type
+            self.logger.error(long_msg)
+            raise CPIActionError(msg, long_msg)
+        return self.repository
+
+
---
+++ src/bosh_ironic_cpi/ironic_cpi/actions/repositories/webdav.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+BOSH OpenStack Ironic CPI
+"""
+# Python 2 and 3 compatibility
+from __future__ import unicode_literals
+
+import logging
+import requests
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    # Python 2
+    from urlparse import urlparse
+
+from ironic_cpi.actions.repositories.repository import RepositoryError
+from ironic_cpi.actions.repositories.repository import Repository
+
+
+
+class WebDavError(RepositoryError):
+
+    def __init__(self, method, url, code, expected=[], msg=None):
+        self._method = method
+        self._url = url
+        self._code = code
+        self._expected = expected
+        if msg:
+            self.message = msg
+        else:
+            self._string()
+    
+    @property
+    def method(self):
+        """Get the error method"""
+        return self._method
+
+    @method.setter
+    def method(self, m):
+        self._method = m
+        self._string()
+
+    @property
+    def url(self):
+        """Get the URL"""
+        return self._url
+
+    @url.setter
+    def url(self, u):
+        self._url = u
+        self._string()
+
+    @property
+    def code(self):
+        """Get the return code"""
+        return self._code
+
+    @code.setter
+    def code(self, c):
+        self._code = c
+        self._string()
+
+    def _string(self):
+        message = "Failed to perform <%s:%s>, return code: '%s'"
+        if self._expected:
+            message = message + ", expected: %s"
+            self.message = message % (method, url, code, self._expected)
+        else:
+            self.message = message % (method, url, code)
+
+
+
+class WebDav(Repository):
+    """
+    Basic implementation of webdav protocol
+    Based on https://github.com/amnong/easywebdav
+    Because of request library, it uses HTTP_PROXY and HTTPS_PROXY env variables
+    """
+    DOWNLOAD_CHUNK_SIZE = 8192
+    protocol = 'webdav'
+
+    def __init__(self, config, logger=None):
+        super(WebDav, self).__init__(config, logger)
+        url = urlparse(config['url'])
+        self.hosturl = url.netloc
+        self.basepath = url.path
+        if not self.basepath.endswith('/'):
+            self.basepath += '/'
+        username = config.get('username', url.username)
+        password = config.get('password', url.password)
+        cert = config.get('cert')
+        timeout = config.get('timeout')
+        self.session = requests.session()
+        self.session.stream = True
+        if cert:
+            self.session.verify = True
+            self.session.cert = cert
+        if username:
+            self.session.auth = (username, password) if password else username
+            self.logger.debug("Using auth with username '%s'" % username)
+        if timeout:
+            self.session.timeout = timeout
+
+    def _send(self, method, path, expected, **kwargs):
+        path = path.strip()
+        if not path.startswith('/'):
+            path = self.basepath + path
+        url = self.hosturl + path
+        self.logger.debug("%s: %s" % (method, url))
+        response = self.session.request(
+            method, url, allow_redirects=False, **kwargs)
+        if response.status_code not in expected:
+            self.logger.error("%s: %s :%" % (method, url, response.status_code))
+            raise WebDavError(method, url, response.status_code, expected)
+        return response
+
+    def mkdir(self, path):
+        codes = [201, 207]
+        dirs = [ dir for dir in path.split('/') if dir ]
+        full_path = ''
+        for d in dirs:
+            full_path = full_path + d + '/'
+            self._send('MKCOL', full_path, codes)
+
+    def rmdir(self, path):
+        codes = [204]
+        path = path.rstrip('/') + '/'
+        self._send('DELETE', path, codes)
+
+    def delete(self, path):
+        codes = [204]
+        self._send('DELETE', path, codes)
+
+    def _upload(self, data, remote):
+        codes = [200, 201, 204]
+        self._send('PUT', remote, codes, data=data)
+
+    def put(self, local, remote):
+        if isinstance(local, basestring):
+            with open(local, 'rb') as f:
+                self._upload(f, remote)
+        else:
+            self._upload(local, remote)
+
+    def _download(self, fd, response):
+        for chunk in response.iter_content(self.DOWNLOAD_CHUNK_SIZE):
+            fd.write(chunk)
+
+    def get(self, remote, local):
+        codes = [200]
+        response = self._send('GET', remote, codes, stream=True)
+        try:
+            if isinstance(local, basestring):
+                with open(local, 'wb') as f:
+                    self._download(f, response)
+            else:
+                self._download(local, response)
+        except EnvironmentError as e:    # parent of IOError, OSError
+            msg = "Cannot write '%s': %s" % (local, e)
+            self.logger.error(msg)
+            raise RepositoryError(msg)
+
+    def exists(self, remote):
+        codes = [200, 404]
+        response = self._send('HEAD', remote, codes)
+        return True if response.status_code != 404 else False
+
+
